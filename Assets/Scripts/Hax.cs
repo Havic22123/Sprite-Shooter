f3c0351ed0fb38794ee2c9d79cccc0d18bd782de
Havic22123 <Jonablox@gmail.com>
2020-01-27 12:19:09 -0700

    Update

--- Assets/Scripts/Hax.cs
+++ Assets/Scripts/Hax.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Hax : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameManager.instance.score += 10;
     }
 }
